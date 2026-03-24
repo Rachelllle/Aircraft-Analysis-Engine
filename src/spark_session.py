2583356a71748c6f5f626480c8309baeaaa0c477
BENOUSSAIDarezkimalek <kkaki5042@gmail.com>
2026-03-24 09:19:44 +0100

    replace print statements with logging for improved traceability in Spark session management

--- src/spark_session.py
+++ src/spark_session.py
@@ -1,4 +1,5 @@
 import os
+from src.logger_config import logger
 os.environ['HADOOP_HOME'] = 'C:/Hadoop'
 os.environ['JAVA_HOME']   = 'C:/Program Files/Java/jdk-19'
 os.environ['PYSPARK_PYTHON'] = 'python'
@@ -6,6 +7,7 @@ os.environ['PYSPARK_PYTHON'] = 'python'
 from pyspark.sql import SparkSession
 
 def get_spark():
+    logger.info('Create Spark session')
     spark = SparkSession.builder \
         .appName('aircraft_classification') \
         .config('spark.driver.memory', '8g') \
@@ -16,5 +18,5 @@ def get_spark():
 
 if __name__ == '__main__':
     spark = get_spark()
-    print('spark ok, version :', spark.version)
+    logger.info('spark ok, version :', spark.version)
     spark.stop()
